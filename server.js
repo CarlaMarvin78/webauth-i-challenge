bcc5b6a82a85c897d79c62ddf2b307ee4265601f
Carla Marvin <carlamarvin78@gmail.com>
2019-09-19 16:26:20 -0400

    MVP complete

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const db = require('./data/users-model');
-
+const dbConfig = require('./data/dbConfig');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
 const server = express();
 
+const sessionConfig = {
+    name: 'oatmeal_raisin',
+    secret: process.env.SESSION_SECRET || 'For the horde',
+    cookie: {
+        maxAge: 1000 * 60 * 60, // in milliseconds
+        secure: false, // true means only send cookie over https
+        httpOnly: true, // true means JS has no access to the cookie
+    },
+    resave: false,
+    saveUninitialized: true, // GDPR compliance
+    store: new KnexSessionStore({
+        knex: dbConfig,
+        tablename: 'knexsessions',
+        sidfieldname: 'sessionid',
+        createtable: true,
+        clearInterval: 1000 * 60 * 30, // clean out expired session data
+    }),
+};
+
 server.use(express.json());
+server.use(session(sessionConfig));
 
 server.post('/api/register', (req, res) => {
     const hash = bcrypt.hashSync(req.body.password, 8);
-    db.add({username: username, password: hash})
+    db.add({username: req.body.username, password: hash})
     .then(user => res.status(201).json(user))
     .catch(err => res.status(500).json(error));
 });
@@ -16,24 +38,29 @@ server.post('/api/register', (req, res) => {
 server.post('/api/login', (req, res) => {
     db.findByUsername(req.body.username)
     .then(user => {
-        if(user!=undefined && bcrypt.compareSync(req.body.password,user.password)) {
-            res.status(200).json({message: "Welcome "+user.username});
+        if(user && bcrypt.compareSync(req.body.password,user.password)) {
+            req.session.user = user;
+            res.status(200).json({message: "Welcome "+user.username+" !"});
         } else {
             res.status(401).json({mesage: 'You shall not pass!'});
         }
     })
     .catch(err => res.status(500).json(err));
 })
+
+server.get('/api/logout', (req, res) => {
+    if (req.session) {
+        req.session.destroy(err => {
+            if (err) {res.status(500).json({error: "Error logging out."})}
+            else {res.status(200).json({message: "Logged out."})}
+        })
+    } else {
+        res.status(200).json({message: "Not logged in."});
+    }
+})
 function validate(req, res, next) {
-    db.findByUsername(req.body.username)
-    .then(user => {
-        if(user!=undefined && bcrypt.compareSync(req.body.password,user.password)) {
-            next();
-        } else {
-            res.status(401).json({mesage: 'You shall not pass!'});
-        }
-    })
-    .catch(err => res.status(500).json(err));
+    if (req.session && req.session.user) {next();}
+    else {res.status(401).json({mesage: 'You shall not pass!'});}
 }
 
 server.get('/api/users', validate, (req, res) => {
